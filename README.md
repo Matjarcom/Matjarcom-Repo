69cf6293b045d247de96ef59bd313620d8031200
Malek <engmalek.civil@gmail.com>
2021-01-24 14:39:18 +0200

    Adding the Content of Pants and T-Shirt page with a simple style

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Matjarcom-Repo
 
 ### Malek Branch
+
+#### Adding the Content of Pants and T-Shirt page with a simple style
---
+++ clothesJS.js
@@ -0,0 +1,108 @@
+"use strict";
+
+var bigContainer = document.getElementById("bigContainer");
+
+function Clothes(img, name, price, gender) {
+  this.img = img;
+  this.name = name;
+  this.price = price;
+  this.gender = gender;
+  Clothes.prototype.allImages.push(this);
+}
+Clothes.prototype.allImages = [];
+
+// Adding Tshirt products
+new Clothes("Clothes_Images/Tshirt/both.png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (2).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (3).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (4).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (5).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (6).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (7).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (8).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (9).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (10).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (11).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (12).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (13).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (14).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (15).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (16).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (17).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/both (18).png", "T-shirt", "23 JD", "Both");
+new Clothes("Clothes_Images/Tshirt/boy.png", "T-shirt", "23 JD", "Boy");
+new Clothes("Clothes_Images/Tshirt/girl.png", "T-shirt", "23 JD", "Girl");
+new Clothes("Clothes_Images/Tshirt/girl (2).png", "T-shirt", "23 JD", "Girl");
+new Clothes("Clothes_Images/Tshirt/girl (3).png", "T-shirt", "23 JD", "Girl");
+new Clothes("Clothes_Images/Tshirt/girl (4).png", "T-shirt", "23 JD", "Girl");
+new Clothes("Clothes_Images/Tshirt/girl (5).png", "T-shirt", "23 JD", "Girl");
+
+// Adding Pants products
+new Clothes("Clothes_Images/pants/boy.png", "Pants", "23 JD", "boy");
+new Clothes("Clothes_Images/pants/boy (2).png", "Pants", "23 JD", "boy");
+new Clothes("Clothes_Images/pants/boy (3).png", "Pants", "23 JD", "boy");
+new Clothes("Clothes_Images/pants/boy (4).png", "Pants", "23 JD", "boy");
+new Clothes("Clothes_Images/pants/boy (5).png", "Pants", "23 JD", "boy");
+new Clothes("Clothes_Images/pants/boy (6).png", "Pants", "23 JD", "boy");
+new Clothes("Clothes_Images/pants/boy (7).png", "Pants", "23 JD", "boy");
+new Clothes("Clothes_Images/pants/boy (8).png", "Pants", "23 JD", "boy");
+new Clothes("Clothes_Images/pants/boy (9).png", "Pants", "23 JD", "boy");
+new Clothes("Clothes_Images/pants/boy (10).png", "Pants", "23 JD", "boy");
+new Clothes("Clothes_Images/pants/boy (11).png", "Pants", "23 JD", "boy");
+new Clothes("Clothes_Images/pants/boy (12).png", "Pants", "23 JD", "boy");
+new Clothes("Clothes_Images/pants/boy (13).png", "Pants", "23 JD", "boy");
+new Clothes("Clothes_Images/pants/boy (14).png", "Pants", "23 JD", "boy");
+new Clothes("Clothes_Images/pants/girl.png", "Pants", "23 JD", "Girl");
+new Clothes("Clothes_Images/pants/girl (2).png", "Pants", "23 JD", "Girl");
+new Clothes("Clothes_Images/pants/girl (3).png", "Pants", "23 JD", "Girl");
+new Clothes("Clothes_Images/pants/girl (4).png", "Pants", "23 JD", "Girl");
+new Clothes("Clothes_Images/pants/girl (5).png", "Pants", "23 JD", "Girl");
+new Clothes("Clothes_Images/pants/girl (6).png", "Pants", "23 JD", "Girl");
+new Clothes("Clothes_Images/pants/girl (7).png", "Pants", "23 JD", "Girl");
+new Clothes("Clothes_Images/pants/girl (8).png", "Pants", "23 JD", "Girl");
+new Clothes("Clothes_Images/pants/girl (9).png", "Pants", "23 JD", "Girl");
+new Clothes("Clothes_Images/pants/girl (10).png", "Pants", "23 JD", "Girl");
+new Clothes("Clothes_Images/pants/girl (11).png", "Pants", "23 JD", "Girl");
+
+for (var i = 0; i <= Clothes.prototype.allImages.length; i++) {
+  // creat a single Product Container and append it in a Big Container
+  var singleProductContainer = document.createElement("div");
+  bigContainer.appendChild(singleProductContainer);
+  singleProductContainer.setAttribute("class", "singleProductContainer");
+
+  // craet image and append it in singleProductContainer
+  var productImage = document.createElement("img");
+  singleProductContainer.appendChild(productImage);
+  productImage.setAttribute("src", Clothes.prototype.allImages[i].img);
+  productImage.setAttribute("width", "300px");
+  productImage.setAttribute("height", "300px");
+
+  // craet the Product Name heading and append it in singleProductContainer
+  var productName = document.createElement("h3");
+  singleProductContainer.appendChild(productName);
+  productName.textContent = Clothes.prototype.allImages[i].name;
+  productName.setAttribute("class", "productNameClass");
+
+  //craet the Product Price heading and append it in singleProductContainer
+  var productPrice = document.createElement("h4");
+  singleProductContainer.appendChild(productPrice);
+  productPrice.textContent = "Price: " + Clothes.prototype.allImages[i].price;
+  productPrice.setAttribute("class", "productPriceClass");
+
+  //craet the Product Gender heading and append it in singleProductContainer
+  var productGender = document.createElement("h4");
+  singleProductContainer.appendChild(productGender);
+  productGender.textContent =
+    "Gender: " + Clothes.prototype.allImages[i].gender;
+  productGender.setAttribute("class", "productGenderClass");
+
+  //creat the button of Add to Cart and append it in singleProductContainer
+  var addToCartButton = document.createElement("input");
+  singleProductContainer.appendChild(addToCartButton);
+  addToCartButton.setAttribute("class", "addToCartButton");
+  addToCartButton.setAttribute("type", "submit");
+  addToCartButton.setAttribute("value", "Add To Cart");
+  var spanOfAddToCartButton = document.createElement("span");
+  addToCartButton.appendChild(spanOfAddToCartButton);
+  spanOfAddToCartButton.textContent = "Add to Cart";
+}
