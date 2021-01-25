6e3e938df7ec89f60bcc9850e0da0569043b44f5
Baraahbsharat <baraaberor@gmail.com>
2021-01-25 21:54:21 +0200

    add

--- js/product.js
+++ js/product.js
@@ -107,15 +107,15 @@ function  createToys(){
 function  createBook(){
     products.prototype.bookArray=[];
 
-    new products('The children','img/books/book1.jpg','book',19.99,false,false,'best baby pants');
-    new products('ice age','img/books/book2.png','book',15,false,false,'beatiful pants jeans');
-    new products('moshi','img/books/book3.jpg','book',15.99,false,false,'best baby pants pob');
-    new products('High five','img/books/book4.jpg','book',9.99,false,false,'best baby red pants');
-    new products('Kiddies','img/books/book5.jpg','book',10,false,false,'best baby red pants');
-    new products('BOX','img/books/book6.jpg','book',12.99,false,false,'best baby red pants');
-    new products('Time to shine','img/books/book7.jpg','book',14.99,false,false,'best baby red pants');
-    new products('Disney','img/books/book8.jpg','book',16,false,false,'best baby red pants');
-    new products('Jump on in ','img/books/book9.png','book',17,false,false,'best baby red pants');
+    new products('Ginger the Girrafe','bookimg/ginger.jpg','book',5.99,false,false,'Read this warm tale of camaraderie and affection set in the wild and beautiful Savannah in our free illustrated kids book. Ginger the giraffe uses her long neck to save the other animals from the blazing forest fire. Follow them in their jungle paths as they all meet with yet another adventure');
+    new products('I found a Frog','bookimg/frog.jpg','book',6,false,false,'Something magical was happening in the fish bowl  and he wasn’t  quite ready for what lay in store.  Read this captivating free illustrated book for kids that encourages them to explore and be awed by the many wonders of nature. ');
+    new products('Hide and seek','bookimg/hide.jpg','book',8.99,false,false,'Was it just another game of hide and seek? No. It was not. First, she fell into a deep, dark hole in the ground and then they found a treasure. Did it end there? No! It did not. Read more about this thrilling adventure of Sally and friends in this free illustrated kids’ book. The fun never ends when Sally’s around!');
+    new products('Doing my chores','bookimg/book4.jpg','book',9.99,false,false,'Love shines through this great illustrated kids’ book . Read how a little girl makes chores fun and easy to do. A fantastic addition to your little one’s free bed time story collection.');
+    new products('Abe the service dog','bookimg/abe.jpg','book',10,false,false,'Abe was a real Service Dog who dedicated his life assisting BJ, a good family friend. Service Dogs are smart, well trained, well behaved, dedicated, and committed to ensuring their master is safe. This book is intended to bring an awareness of their importance to early readers.');
+    new products('Tooth fairy','bookimg/tooth.jpg','book',10.99,false,false,'A wonderful story that weaves its magic in the child’s imagination, even while imparting an invaluable lesson in oral health and hygiene. This is a story that the children will return to time and again for a fun bedtime read.');
+    new products(' Captin FAantastic','bookimg/captin.png','book',10.99,false,false,'Dr. Zob’s evil plans had failed once again, thanks to his wonder dog, Winston. They were just were they had to be, with Princess Aura at her birthday party. Read on to find out why Winston deserves a big cuddle and a juicy big bone. A big ‘Woof!’ to this wonderful free children’s story book.');
+    new products('The case of the missing smile','mookimg/jpg.jpg','book',7,false,false,'Detective Peterson is trying to find where Sally Sue lost her beautiful smile. And she finds it..right in her heart, hidden from sight. Read this imaginative story of a little girl and how she learnt to smile again in this great free children’s fairy story.');
+    new products('hammy the hamster','bookimg/hammy.jpg','book',8,false,false,'He’s got swag. He wears a tux. He’s a hipster hamster who likes to live free. Join Hammy in his South American home as you read this free story book for kids on your next read aloud day');
     renderItems(products.prototype.bookArray);
     localStorage.setItem('render',JSON.stringify(products.prototype.bookArray));
 }
